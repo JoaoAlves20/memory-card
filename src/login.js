15ec0bb516aa6e5b3e3b6077b0781ed00e39b730
JoaoAlves20 <jvsimoesalves@gmail.com>
2025-01-13 08:08:09 -0300

    feat: created pagination and saving in local storage

--- src/login.js
+++ src/login.js
@@ -1,7 +1,16 @@
 const input = document.querySelector(".login_input");
 const button = document.querySelector(".login_button");
+const form = document.querySelector(".login_form");
 
 const validationInput = ({ target }) => target.value.length > 2 ? (
     button.removeAttribute("disabled")) : button.setAttribute("disabled", "");
 
+const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    localStorage.setItem("player", input.value);
+    window.location = "pages/game.html";
+};
+
 input.addEventListener("input", validationInput);
+form.addEventListener("submit", handleSubmit)
